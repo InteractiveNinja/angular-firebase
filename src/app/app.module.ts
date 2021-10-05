c27d41b1ce203ae5ac27bcd9f40d4f1f925c3369
Gabriel Nadolny <gabriel.nadolny@coop.ch>
2021-10-05 08:58:17 +0200

    prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
-import { Store } from 'store';
+import { Store } from '../store';
 
 // feature modules
 
@@ -15,18 +15,9 @@ import { AppComponent } from './containers/app/app.component';
 export const ROUTES: Routes = [];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(ROUTES)
-  ],
-  declarations: [
-    AppComponent
-  ],
-  providers: [
-    Store
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES)],
+  declarations: [AppComponent],
+  providers: [Store],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBfpmnft7o5JU3dbi9FsL9-wtcEim1cw7A',
+    authDomain: 'fullplan.firebaseapp.com',
+    databaseURL: 'https://fullplan-default-rtdb.firebaseio.com',
+    projectId: 'fullplan',
+    storageBucket: 'fullplan.appspot.com',
+    messagingSenderId: '199594310507',
+    appId: '1:199594310507:web:08dd3b4c0c32f6864c2a2e',
+    measurementId: 'G-LNPVW8SEGN',
+  },
 };
 
 /*
